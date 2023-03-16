6c63016c12a3446f2e6d3a18daa1ee0f11b21782
Barished <shedrackipi@gmail.com>
2023-03-16 16:27:31 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with puts
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

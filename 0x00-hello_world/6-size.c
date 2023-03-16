db9d3198cc396fcadbea5e556371aacd0a87d6ec
Barished <shedrackipi@gmail.com>
2023-03-16 16:54:29 -0700

    Size is not a grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the size of various computer data types
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+	printf("size of char: %i byte(s)\n",(unsigned long)sizeof(char));
+	printf("size of int: %i byte(s)\n",(unsigned long)sizeof(int));
+	printf("size of long int: %i byte(s)\n",(unsigned long)sizeof(long int));
+	printf("size of long long: %i byte(s)\n",(unsigned long)sizeof(long long));
+	printf("size of float: %i byte(s)\n",(unsigned long)sizeof(float));
+	return (0);
+}

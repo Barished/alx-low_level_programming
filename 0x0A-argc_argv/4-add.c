51141e4978dd07f2015c845bca54fc5bbfc291ff
Barished <shedrackipi@gmail.com>
2023-04-09 18:05:29 +0000

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ *check_num - check - string there are digit
+ *@str: array str
+ *
+ *Return: Always 0 (Success)
+ */
+
+int check_num(char *str)
+{
+	unsigned int ball;
+
+	ball = 0;
+	while (ball < strlen(str))
+	{
+		if (!isdigit(str[ball]))
+		{
+			return (0);
+		}
+		ball++;
+	}
+	return (1);
+}
+
+/**
+ *main - Prints the name of the programe
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int ball;
+	int str;
+	int sum = 0;
+
+	ball = 1;
+	while (ball < argc)
+	{
+		if (check_num(argv[ball]))
+		{
+			str = atoi(argv[ball]);
+					sum += str;
+					}
+					else
+					{
+					printf("Error\n");
+					return (1);
+					}
+					ball++;
+					}
+					printf("%d\n", sum);
+					return (0);
+					}

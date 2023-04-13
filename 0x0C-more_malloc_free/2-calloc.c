8501a915804b9290d85bc664ee9c70f7a5a74580
Barished <shedrackipi@gmail.com>
2023-04-13 22:36:06 +0000

    string noncat and calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,30 @@
-e
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_calloc - allocates memory for an array of nmemb elements
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, l = 0;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	l = nmemb * size;
+	ptr = malloc(l);
+
+	if (ptr == NULL)
+		return (NULL);
+	while (i < l)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,1 +1,26 @@
-e
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *array_range - creates an array of integers
+ * @min: minimum range of values
+ * @max: maximum range of values
+ *
+ * Return: pointer to the new array
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int q, size;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	ptr = malloc(sizeof(int) * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (q = 0; min <= max; q++)
+		ptr[q] = min++;
+	return (ptr);
+}

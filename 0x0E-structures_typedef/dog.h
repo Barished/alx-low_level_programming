4e51c771744fd696938a05566b643f93a4432de7
Barished <shedrackipi@gmail.com>
2023-04-17 16:38:52 +0000

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ struct dog
  *dog_t - typedef for struct dog
  */
 
-typedef struct dog dog_t;
+struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
